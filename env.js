cc9e0dcb7e104a6673eeaeaac44080a86ab9568d
SasquatchPunter <jeremyrobertelliott@gmail.com>
2026-03-15 17:33:08 -0600

    feat: add better env init and parsing

--- env.js
+++ env.js
@@ -1,3 +1,34 @@
+import { exit } from "process";
 import { config } from "dotenv";
 
-config();
+const dotenv = config({ quiet: true }).parsed ?? {};
+
+/**
+ * Resolver that maintains precedence of env variables.
+ * If `fallback` parameter is undefined, the resolver throws an error.
+ * @param {string} name
+ * @param {string | undefined} fallback
+ */
+function resolve(name, fallback) {
+  const e = process.env[name] ?? dotenv[name] ?? fallback;
+
+  if (fallback === undefined && e === undefined)
+    throw TypeError(`Missing environment variable ${name}`);
+
+  return e;
+}
+
+const env = {};
+
+try {
+  env.API_KEY = resolve("API_KEY");
+  env.API_ENDPOINT = resolve(
+    "API_ENDPOINT",
+    "https://texttospeech.googleapis.com/v1beta1/text:synthesize",
+  );
+} catch (err) {
+  console.log(err.message);
+  exit(1);
+}
+
+export default env;
