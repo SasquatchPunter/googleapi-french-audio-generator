5004b0e118b3fef5e3c6fa8f67c81333f887e611
SasquatchPunter <jeremyrobertelliott@gmail.com>
2026-04-23 01:15:57 -0600

    feat: add batch module

---
+++ src/lib/__tests__/batch.test.js
@@ -0,0 +1,57 @@
+const { chunkTasks, processTaskQueue, delay } = require("../batch.js");
+
+describe("chunkRequests()", () => {
+  test("dequeues Promises", () => {
+    const task = () => new Promise(() => {});
+    const queue = [task, task, task];
+    chunkTasks(queue, 2);
+    expect(queue).toHaveLength(1);
+    chunkTasks(queue, 1);
+    expect(queue).toHaveLength(0);
+  });
+
+  test("returns empty batch when empty queue passed", () => {
+    expect(chunkTasks([], 1)).toHaveLength(0);
+  });
+
+  test("batches remainder when concurrent limit is more than pool.length", () => {
+    const task = () => new Promise(() => {});
+    const queue = [task, task, task];
+    expect(chunkTasks(queue, 2)).toHaveLength(2);
+    expect(chunkTasks(queue, 2)).toHaveLength(1);
+    expect(chunkTasks(queue, 2)).toHaveLength(0);
+  });
+});
+
+describe("processTaskQueue()", () => {
+  const resolveTask = () =>
+    new Promise((resolve) => {
+      resolve();
+    });
+  const rejectTask = () =>
+    new Promise((_, reject) => {
+      reject();
+    });
+
+  test("drains a queue fully", async () => {
+    const queue = [resolveTask, resolveTask, resolveTask];
+    const rejected = await processTaskQueue(queue, 1, 500);
+    expect(queue).toHaveLength(0);
+    expect(rejected).toHaveLength(0);
+  });
+
+  test("rejected tasks are returned", async () => {
+    const queue = [resolveTask, rejectTask, resolveTask];
+    const rejected = await processTaskQueue(queue, 1, 500);
+    expect(rejected).toHaveLength(1);
+  });
+});
+
+describe("delay()", () => {
+  test("awaits ", async () => {
+    const d = 100;
+    let time = Date.now();
+    const res = await delay(d).then(() => Date.now() - time);
+    expect(res).toBeGreaterThanOrEqual(d);
+  });
+});
---
+++ src/lib/batch.js
@@ -0,0 +1,74 @@
+/**
+ * Chunks HTTP requests from a queue.
+ * @param {(() => Promise<any>)[]} queue
+ * @param {number} length
+ */
+function chunkTasks(queue, length) {
+  const chunk = [];
+  while (length-- > 0 && queue.length > 0) {
+    chunk.push(queue.pop());
+  }
+  return chunk;
+}
+
+/**
+ * Batches and concurrently processes a queue of tasks.
+ *
+ * Each task is a "runner" function returning a Promise. This is to hold off executing
+ * the function passed to the return Promise until the task function is ready to be run.
+ *
+ * Processed tasks are removed from the queue.
+ * Tasks whose promises reject are returned after the queue has been fully processed.
+ * @param {(() => Promise<any>)[]} queue Queue of tasks to batch and process.
+ * @param {number} concurrent Number of tasks to run during every timeout window.
+ * @param {number} timeoutMS Length of timeout period in milliseconds.
+ * @returns {Promise<(() => Promise<any>)[]>} An array of rejected tasks.
+ */
+
+async function processTaskQueue(queue, concurrent, timeoutMS) {
+  /** @type {(() => Promise<any>)[]} */
+  const rejected = [];
+  const loopDelayMS = 100;
+  let pending = 0;
+  let lastTimeout = 0;
+
+  while (true) {
+    if (queue.length == 0 && pending === 0) break;
+
+    const now = Date.now();
+
+    if (now - lastTimeout > timeoutMS) {
+      lastTimeout = now;
+
+      const chunk = chunkTasks(queue, concurrent).map((task) =>
+        task().catch(() => {
+          rejected.push(task);
+        }),
+      );
+
+      pending++;
+
+      Promise.allSettled(chunk).finally(() => {
+        pending--;
+      });
+    }
+
+    await delay(loopDelayMS);
+  }
+
+  return rejected;
+}
+
+/**
+ * Promise that resolves after a delay. Can be used for throttling in async functions.
+ * @param {number} delayMS Timeout in milliseconds.
+ */
+async function delay(delayMS = 0) {
+  return await new Promise((resolve) => setTimeout(resolve, delayMS));
+}
+
+module.exports = {
+  chunkTasks,
+  processTaskQueue,
+  delay,
+};
